3e728a16e27ff5a33b0d573d017e438162dc1b92
Ramadan <daudalaramadan2@gmail.com>
2024-03-03 02:47:35 +0000

    handling the drawer at state

--- my-app/src/shared/Navigation/MainNavigation.jsx
+++ my-app/src/shared/Navigation/MainNavigation.jsx
@@ -1,31 +1,37 @@
-import React from 'react'
+import React , { useState }from 'react'
 import './MainNavigation.css'
 import MainHeader from './MainHeader'
 import { Link } from 'react-router-dom'
 import NavLinks  from './NavLinks'
 import SideDrawer from './SideDrawer'
 
+
 const MainNavigation = (props) => {
+    const [drawerIsOpen, setdrawerIsOpen] = useState(false)
   return (
     <React.Fragment>
-    <SideDrawer>
-        <nav className='main-navigation__drawer-nav'>
-            <NavLinks />
-        </nav>
-    </SideDrawer>
-    <MainHeader>
-        <button className='main-navigation__menu-btn'>
-            <span className='span'/>
-            <span />
-            <span />
-        </button>
-        <h1 className='main-navigation__title'>
-            <Link to="/">YourPlaces</Link>
-        </h1>
-        <nav className='main-navigation__header-nav'>
-            <NavLinks />
-        </nav>
-    </MainHeader>
+        
+        {drawerIsOpen ? (
+            <SideDrawer>
+                <nav className='main-navigation__drawer-nav'>
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+        ) : null}
+        <MainHeader>
+            <button className='main-navigation__menu-btn'>
+               <span className='span'/>
+               <span />
+               <span />
+            </button>
+            <h1 className='main-navigation__title'>
+                <Link to="/">YourPlaces</Link>
+            </h1>
+            <nav className='main-navigation__header-nav'>
+                <NavLinks />
+            </nav>
+        </MainHeader>
+    
     </React.Fragment>
   )
 }
